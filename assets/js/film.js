baff08b195763cda6ef63a3391fefc7bcbd707b7
XiangLi1412 <63719557+XiangLi1412@users.noreply.github.com>
2020-04-23 12:56:40 +0800

    Update film.js

--- assets/js/film.js
+++ assets/js/film.js
@@ -48,7 +48,7 @@ const c_joke = () => {
 		joke_text = "../assets/img/film/"+joke_num+".jpg";
 		let joke = document.getElementById("joke");
 		joke.src = joke_text;
-	},220);
+	},210);
 }
 
 c_joke();
